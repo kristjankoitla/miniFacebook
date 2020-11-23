0fe8ba281d3d27e242307b980dae7e5bd6f21420
Kristjan Koitla <koitlakristjan@gmail.com>
2020-11-23 18:53:01 +0200

    Add posts to homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,9 +11,14 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @param PostRepository $repository
+     * @return Response
      */
-    public function index(): Response
+    public function index(PostRepository $repository): Response
     {
-        return $this->render('home/index.html.twig');
+        $posts = $repository->findAll();
+        return $this->render('home/index.html.twig', [
+            'posts' => $posts
+        ]);
     }
 }
