fe4d2b33bccbb30ed792292f6073f06851e0f6f4
Kristjan Koitla <koitlakristjan@gmail.com>
2020-11-23 19:32:17 +0200

    Add registration and loging

---
+++ src/Form/UserType.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'required' => true,
+                'first_options' => ['label' => 'Password'],
+                'second_options' => ['label' => 'Confirm Password']
+            ])
+            ->add('register', SubmitType::class, [
+                'attr' => [
+                    'class' => 'button is-primary'
+                ]
+            ]);
+    }
+}
