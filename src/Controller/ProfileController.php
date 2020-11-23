77939ee2e26806cbc6b341aece9d99c831428e5e
Kristjan Koitla <koitlakristjan@gmail.com>
2020-11-23 20:15:06 +0200

    Add profile page

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class ProfileController extends AbstractController
+{
+    /**
+     * @Route("/profile", name="profile")
+     * @param UserInterface $user
+     * @return Response
+     */
+    public function index(UserInterface $user): Response
+    {
+        return $this->render('profile/index.html.twig', [
+            'user' => $user
+        ]);
+    }
+}
