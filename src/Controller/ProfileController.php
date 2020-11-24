654d3a155bd62aa3b930376a08058c6fc5071530
Kristjan Koitla <koitlakristjan@gmail.com>
2020-11-24 19:14:36 +0200

    Add functionality for editing profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,16 +2,22 @@
 
 namespace App\Controller;
 
+use App\Form\ProfileEditType;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+
+/**
+ * @Route("/profile", name="profile.")
+ */
 class ProfileController extends AbstractController
 {
     /**
-     * @Route("/profile", name="profile")
+     * @Route("/", name="index")
      * @param UserInterface $user
      * @param PostRepository $repository
      * @return Response
@@ -23,6 +29,30 @@ class ProfileController extends AbstractController
         return $this->render('profile/index.html.twig', [
             'user' => $user,
             'posts' => $posts
+        ]);
+    }
+
+    /**
+     * @Route("/edit", name="edit")
+     * @param Request $request
+     * @param UserInterface $user
+     * @return Response
+     */
+    public function edit(Request $request, UserInterface $user): Response
+    {
+        $form = $this->createForm(ProfileEditType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($user);
+            $em->flush();
+        }
+
+        return $this->render('profile/edit.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
