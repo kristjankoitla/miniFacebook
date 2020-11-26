8573a5ce759a445a0a24e23f4db4f3f271835f1e
Kristjan Koitla <koitlakristjan@gmail.com>
2020-11-26 09:56:19 +0200

    Remove traces of being able to delete stuff.. you can't.. get over it

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\Comment;
 use App\Entity\Like;
 use App\Entity\Post;
-use App\Form\PostType;
 use App\Repository\CommentRepository;
 use App\Repository\LikeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,40 +13,12 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @Route("/post", name="post.")
  */
 class PostController extends AbstractController
 {
-
-    /**
-     * @Route("/create", name="create")
-     * @param Request $request
-     * @param UserInterface $user
-     * @return Response
-     */
-    public function create(Request $request, UserInterface $user)
-    {
-        $post = new Post();
-        $post->setUser($user);
-
-        $form = $this->createForm(PostType::class, $post);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted()) {
-            $em = $this->getDoctrine()->getManager();
-
-            $em->persist($post);
-            $em->flush();
-        }
-
-        return $this->render('post/create.html.twig', [
-            'form' => $form->createView()
-        ]);
-    }
 
     /**
      * @Route("/show/{id}", name="show")
@@ -122,17 +93,4 @@ class PostController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/remove/{id}", name="remove")
-     * @param Post $post
-     */
-    public function delete(Post $post)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $em->remove($post);
-        $em->flush();
-
-        return $this->redirect($this->generateUrl('home'));
-    }
 }
