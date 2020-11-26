84c77796de997342871897b464044bf363c40996
Kristjan Koitla <koitlakristjan@gmail.com>
2020-11-26 09:49:23 +0200

    Creating posts

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Repository\CommentRepository;
 use App\Repository\LikeRepository;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,11 +17,34 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @param Request $request
      * @param PostRepository $postRepository
+     * @param LikeRepository $likeRepository
+     * @param CommentRepository $commentRepository
      * @return Response
      */
-    public function index(PostRepository $postRepository, LikeRepository $likeRepository, CommentRepository $commentRepository): Response
+    public function index(Request $request, PostRepository $postRepository, LikeRepository $likeRepository, CommentRepository $commentRepository): Response
     {
+        $form = $this->createFormBuilder()
+            ->add("post", TextareaType::class)
+            ->add('submit', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $post = new Post();
+            $post->setUser($this->getUser());
+            $post->setText($form->getData()['post']);
+
+            $em->persist($post);
+            $em->flush();
+
+            return $this->redirect($request->getUri());
+        }
+
         $posts = $postRepository->findFriendsPosts($this->getUser());
         foreach ($posts as $post) {
             $commentCount = count($commentRepository->findCommentsOnPost($post));
@@ -27,7 +54,8 @@ class HomeController extends AbstractController
         }
 
         return $this->render('home/index.html.twig', [
-            'posts' => $posts
+            'posts' => $posts,
+            'form' => $form->createView()
         ]);
     }
 }
