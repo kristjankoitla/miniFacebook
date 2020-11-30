97c07ef3cb2e4cac27cac3d27215e4d5cec5bcd6
Kristjan Koitla <koitlakristjan@gmail.com>
2020-11-30 18:53:23 +0200

    Fix search js

--- public/base.js
+++ public/base.js
@@ -2,6 +2,6 @@ let search = document.getElementById("search");
 search.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
         let query = search.value.split(' ').join('').toLowerCase();
-        window.location.replace('/search?string=' + query)
+        window.location.replace('/~krkoit/prax3/search?string=' + query)
     }
 })
