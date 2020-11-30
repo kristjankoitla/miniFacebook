63f1fdbf0036350908b1bb2da4f69e9fe5c3da31
Kristjan Koitla <koitlakristjan@gmail.com>
2020-11-30 20:24:45 +0200

    Make the navbar work on mobile devices

--- public/base.js
+++ public/base.js
@@ -5,3 +5,29 @@ search.addEventListener('keypress', function (e) {
         window.location.replace('/~krkoit/prax3/search?string=' + query)
     }
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+
+        // Add a click event on each of them
+        $navbarBurgers.forEach( el => {
+            el.addEventListener('click', () => {
+
+                // Get the target from the "data-target" attribute
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
+
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+
+            });
+        });
+    }
+
+});
